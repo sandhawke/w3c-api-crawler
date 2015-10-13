bf555ca465fe5296fcf81107e137f9c981a9601a
Sandro Hawke <sandro@hawke.org>
2015-10-12 20:26:40 -0400

    remove API key

--- crawler.js
+++ crawler.js
@@ -3,9 +3,9 @@
   Crawl something like api.w3.org to produce w3cdata.json, which has
   the same data in one structure.
 
-	BUGS?
-	
-	-- with 10k fetches done, I only have 7301 files on disk.  How is that?
+  BUGS?
+  
+  -- with 10k fetches done, I only have 7301 files on disk.  How is that?
 
 */
 'use strict'
@@ -16,8 +16,8 @@ const fetcher = require('./fetcher')
 var debug = require('debug')('crawler')
 
 let headers = {
-	Authorization: 'W3C-API apikey="3fv3wwwplyg4skgocsoocc4g0wkcsc8"',
-	Accept: 'text/json'
+  Authorization: 'W3C-API apikey="'+process.env.W3CAPIKEY+'"',
+  Accept: 'text/json'
 }
 
 let f = new fetcher.Fetcher({addHeaders:headers, escapeSlash:false, interval:50})
@@ -75,82 +75,82 @@ const exampleContainer = {
     }
 
 /*   
-		 Is this a "container" page?  Has the side effect, if it is, of
-		 adding ._items and .itemProperty to make it easier to deal with
-		 in the future.
-		 
-		 Container pages look something like exampleContainer, above
-		 
-		 In this example, the .itemProperty would be set to "services".
+     Is this a "container" page?  Has the side effect, if it is, of
+     adding ._items and .itemProperty to make it easier to deal with
+     in the future.
+     
+     Container pages look something like exampleContainer, above
+     
+     In this example, the .itemProperty would be set to "services".
 */
 let isContainer = (page) => {
-	if (page._items) {
-		// already flagged
-		return true
-	}
-	let keys = Object.keys(page)
-	if (keys.length === 6 &&    // there's the .id we added
-			page._links !== undefined &&
-			page.limit !== undefined &&
-			page.page !== undefined &&
-			page.pages !== undefined &&
-			page.total !== undefined) {
-		// it's got all the right properties.  
-		let prop = null
-		for (let key of Object.keys(page._links)) {
-			if (key === "self" || key === "first" || key === "last" ||
-					key === "next" || key === "previous" || key === "up") {
-				continue
-			}
-			if (prop === null) {
-				prop = key
-			} else {
-				console.log("WARNING: Extra keys, can't figure out which is the iteration property:", prop, key)
-			}
-		}
+  if (page._items) {
+    // already flagged
+    return true
+  }
+  let keys = Object.keys(page)
+  if (keys.length === 6 &&    // there's the .id we added
+      page._links !== undefined &&
+      page.limit !== undefined &&
+      page.page !== undefined &&
+      page.pages !== undefined &&
+      page.total !== undefined) {
+    // it's got all the right properties.  
+    let prop = null
+    for (let key of Object.keys(page._links)) {
+      if (key === "self" || key === "first" || key === "last" ||
+          key === "next" || key === "previous" || key === "up") {
+        continue
+      }
+      if (prop === null) {
+        prop = key
+      } else {
+        console.log("WARNING: Extra keys, can't figure out which is the iteration property:", prop, key)
+      }
+    }
 
-		// prop MIGHT be null from api.w3.org.   I guess this is one way
-		// and empty list is done.
-		// 'https://api-test.w3.org/groups/71714/chairs?page=1&items=100'
+    // prop MIGHT be null from api.w3.org.   I guess this is one way
+    // and empty list is done.
+    // 'https://api-test.w3.org/groups/71714/chairs?page=1&items=100'
 
-		debug('decided iteration property is', prop)
-		page._itemProperty = prop
-		page._items = []
-		return true
-	} else {
-		if (page.page) {
-			throw "ERROR: page.page on a non-container!  Suspicious."
-		}
-		return false
-	}
+    debug('decided iteration property is', prop)
+    page._itemProperty = prop
+    page._items = []
+    return true
+  } else {
+    if (page.page) {
+      throw "ERROR: page.page on a non-container!  Suspicious."
+    }
+    return false
+  }
 }
 
 let gatherPages = (thisPage, firstPage, cb) => {
 
-	if (firstPage._itemProperty === null) {
-		cb()
-		return
-	}
-	
-	// add the items on this page to _items on the firstpage
-	let newItems = thisPage._links[firstPage._itemProperty]
-	//debug('gather', firstPage._itemProperty, thisPage)
-	debug('got', newItems.length,'items on page', thisPage.page, 'of', thisPage.pages)
-	Array.prototype.push.apply(firstPage._items, newItems)
-														 
+  if (firstPage._itemProperty === null) {
+    cb()
+    return
+  }
+  
+  // add the items on this page to _items on the firstpage
+  let newItems = thisPage._links[firstPage._itemProperty]
+  //debug('gather', firstPage._itemProperty, thisPage)
+  debug('got', newItems.length,'items on page', thisPage.page, 'of', thisPage.pages)
+  Array.prototype.push.apply(firstPage._items, newItems)
+                             
 
-	// if there is a next page, recursively gather there, too
-	let next = thisPage._links.next
-	if (next) {
-		next = next.href
-		debug('there\'s a next page', next)
-		f.fetch(next, (result) => {
-			gatherPages(result.data, firstPage, cb)
-		})
-	} else {
-		// or if there is no next page, we're finally done
-		cb()
-	}
+  // if there is a next page, recursively gather there, too
+  let next = thisPage._links.next
+  if (next) {
+    next = next.href
+    debug('there\'s a next page', next)
+    f.fetch(next, (result) => {
+      gatherPages(result.data, firstPage, cb)
+    })
+  } else {
+    // or if there is no next page, we're finally done
+    cb()
+  }
 
 }
 
@@ -160,90 +160,90 @@ let gatherPages = (thisPage, firstPage, cb) => {
 // cb
 let fill = (obj, cb) => {
 
-	let href = obj.href
-	if (pages[href]) {
-		// dont call the callback; that's how we signal already done
-		return
-	}
-	if (href.slice(0,siteURL.length) !== siteURL) {
-		debug('skipping off-site URL', href)
-		return
-	}
-	
-	pages[href] = "in progress"
+  let href = obj.href
+  if (pages[href]) {
+    // dont call the callback; that's how we signal already done
+    return
+  }
+  if (href.slice(0,siteURL.length) !== siteURL) {
+    debug('skipping off-site URL', href)
+    return
+  }
+  
+  pages[href] = "in progress"
 
-	f.fetch(href, (result) => {
-		let page = result.data
-		if (typeof page !== "object") {
-			debug('page not object', href, typeof page)
-			return
-		}
-		if (page === null) {
-			// like a 404, it went away, huh...
-			return
-		}
-		page.id = href
-		//debug('got data', page)
+  f.fetch(href, (result) => {
+    let page = result.data
+    if (typeof page !== "object") {
+      debug('page not object', href, typeof page)
+      return
+    }
+    if (page === null) {
+      // like a 404, it went away, huh...
+      return
+    }
+    page.id = href
+    //debug('got data', page)
 
-		if (isContainer(page)) {
-			gatherPages(page, page, ()=> {
-				pages[href] = page
-				cb(page)
-			})
-		} else {
-			pages[href] = page
-			cb(page)
-		}
-	})
+    if (isContainer(page)) {
+      gatherPages(page, page, ()=> {
+        pages[href] = page
+        cb(page)
+      })
+    } else {
+      pages[href] = page
+      cb(page)
+    }
+  })
 }
 
 /*
 if (isContainer(exampleContainer)) {
-	debug('exampleContainer: ', exampleContainer)
+  debug('exampleContainer: ', exampleContainer)
 }
 if (isContainer({page:3})) {
 }
 */
 
 let filled = (page) => {
-	console.log('filled', page.id)
-	if (page._items) {
-		debug('page filled, doing items', page.id)
-		for (let item of page._items) {
-			fill(item, filled)
-		}
-	} else {
-		let links = page._links
-		if (links) {
-			debug('page filled, doing links', page.id, page)
-			for (let prop of Object.keys(links)) {
-				debug('prop', prop)
-				let val = links[prop]
-				debug('link', prop, val)
-				if (Array.isArray(val)) {
-					debug('iterating over array value')
-					for (let item of val) {
-						fill(item, filled)
-					}
-				} else {
-					fill(val, filled)
-				}
-			}
-		} else {
-			debug('page filled, has no links', page)
-		}
-	}
+  console.log('filled', page.id)
+  if (page._items) {
+    debug('page filled, doing items', page.id)
+    for (let item of page._items) {
+      fill(item, filled)
+    }
+  } else {
+    let links = page._links
+    if (links) {
+      debug('page filled, doing links', page.id, page)
+      for (let prop of Object.keys(links)) {
+        debug('prop', prop)
+        let val = links[prop]
+        debug('link', prop, val)
+        if (Array.isArray(val)) {
+          debug('iterating over array value')
+          for (let item of val) {
+            fill(item, filled)
+          }
+        } else {
+          fill(val, filled)
+        }
+      }
+    } else {
+      debug('page filled, has no links', page)
+    }
+  }
 }
 
 let file = 'w3c-api-pages.json'
 let save = () => {
-	debug("writing", file)
-	fs.writeFile(file,
-							 JSON.stringify(pages,null,4),
-							 ()=>{	
-								 console.log(file, "written")
-								 process.exit()
-							 });
+  debug("writing", file)
+  fs.writeFile(file,
+               JSON.stringify(pages,null,4),
+               ()=>{  
+                 console.log(file, "written")
+                 process.exit()
+               });
 
 }
 
